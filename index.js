40b19bf5be27138641c94b551d9353392911de95
Old Prego <spymasterluke@gmail.com>
2021-04-07 11:46:10 -0500

    Added Readme for goal tracking and general index.js format

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+    ])
+    .then((data) => {
+        const filename = `${data.name.toLowerCase().split(' ').join('')}.html`;
+        console.log(data);
+
+        fs.writeFile(filename,htmlData, (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
