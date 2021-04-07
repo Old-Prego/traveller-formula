1802ced3ff5a043a44fbdbfd8ae4d70ada26864c
Old Prego <spymasterluke@gmail.com>
2021-04-07 12:24:49 -0500

    Got basic file writing correctly, now working on the syntaxes of the formula to make sure it is created properly

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { ifError } = require('assert');
 
 inquirer
     .prompt([
         {
             type: 'input',
-            name: 'name',
-            message: 'What is your name?',
+            name: 'fileName',
+            message: 'What would you like the output file named?',
+        },
+        {
+            type: 'input',
+            name: 'sheet',
+            message: 'What sheet should the vlookup reference?',
+        },
+        {
+            type: 'input',
+            name: 'column',
+            message: 'What column should the vlookup reference?',
+        },
+        {
+            type: 'input',
+            name: 'startRow',
+            message: 'What row should the vlookup start at?',
+        },
+        {
+            type: 'input',
+            name: 'rowCount',
+            message: 'What is the ending row for the vlookups?',
+        },
+        {
+            type: 'input',
+            name: 'retCol',
+            message: 'Which column should be returned from the vlookup (number)?',
         },
     ])
     .then((data) => {
-        const filename = `${data.name.toLowerCase().split(' ').join('')}.html`;
-        console.log(data);
 
-        fs.writeFile(filename,htmlData, (err) =>
+        var funcString;
+        var ifErrorString;
+
+        var rowCount = data.rowCount - data.startRow;
+        
+        for (var i = data.startRow; i < rowCount; i++){
+
+            if (funcString == undefined){
+                funcString = "VLOOKUP(" + data.sheet + "!$" + data.column + "$" + i + ",I:I," + data.retCol + ",0),";
+                ifErrorString = "IFERROR(";
+            } else {
+                funcString = "VLOOKUP(" + data.sheet + "!$" + data.column + "$" + i + ",I:I," + data.retCol + ",0)," + funcString;
+                ifErrorString = "IFERROR(" + ifErrorString;
+            }
+
+
+
+        }
+
+        const fileName = data.fileName + ".txt";
+
+        fs.writeFile(fileName,finFunc, (err) =>
             err ? console.log(err) : console.log('Success!')
         );
     });
